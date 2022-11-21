3ae9cf90d031f0b317c6907c642e21b3242aa50d
yadhukrishnatm <yadhu3june@gmail.com>
2022-11-21 17:45:22 +0530

    commit

---
+++ nov project 4.dart
@@ -0,0 +1,9 @@
+void main(){
+  String name = "yadhu";
+  int age     = 20;
+  double mark = 8.5;
+
+  print("my details");
+  print("name : $name");
+  print("age : $age");
+  print("mark : $mark");
