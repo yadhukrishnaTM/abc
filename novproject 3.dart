12248e2cecf33b5a7b56cbad18149bf17a944c62
yadhukrishnatm <yadhu3june@gmail.com>
2022-11-21 16:56:28 +0530

    commit

---
+++ novproject 3.dart
@@ -0,0 +1,15 @@
+ import 'package:test/expect.dart';
+
+void main(){
+  String name = "yadhu";
+  int age     = 20;
+  double mark = 8.5;
+
+  print("my details");
+  print("name : $name");
+  print("age : $age");
+  print("mark : $mark");
+
+
+
+}
