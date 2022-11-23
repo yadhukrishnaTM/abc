1ee36094eeb3dd5b15e1a3ffd5a07fa74672aba4
yadhukrishnatm <yadhu3june@gmail.com>
2022-11-23 16:16:55 +0530

    commit

---
+++ novproject 7.dart
@@ -0,0 +1,13 @@
+void main() {
+  var a = 10;
+  var b = 20;
+  // var res = a/b;
+
+  print('a+b    = ${a + b}');
+  print('a-b    = ${a - b}');
+  print('a*b    = ${a * b}');
+  print('a/b    = ${a / b}');
+  print('a~/b   = ${a ~/ b}');
+  print('a%b    = ${a % b}');
+  print('-(a-b) = ${-(a - b)}');
+}
