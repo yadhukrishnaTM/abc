6862ee070818c51a9dfc982806559dc06d18fbef
yadhukrishnatm <yadhu3june@gmail.com>
2022-12-01 19:36:04 +0530

    commit

---
+++ factorial.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  print('enter a number');
+  int N = int.parse(stdin.readLineSync()!);
+
+  int result = 1;
+  for(int i=1;i<=N;i++){
+   result=result*i;
+
+
+  }
+  print('factorial of $result');
+  print(result);
+
+
+}
+
